f57457ef6060b760684f2679f6e84fe795ebb082
nbarnettdesign <nbarnett.design@gmail.com>
2017-05-16 12:15:03 +1000

    First Backup Of file, Added player movement (Basic) Sneak (Basic) camera follow, materials for ground and player and enemy

---
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class PlayerController : MonoBehaviour {
+
+
+	public float walkSpeed = 1.2f;
+
+	// Update is called once per frame
+	void Update () {
+		//Basic wasd to move in the directions at the speed walkSpeed
+		if(Input.GetKey(KeyCode.W)){
+			transform.position += transform.forward * walkSpeed;
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.position -= transform.forward * walkSpeed;
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.position += (new Vector3 (-1, 0, 0) * walkSpeed);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.position += (new Vector3 (1, 0, 0) * walkSpeed);
+		}
+		//Sneak Function, need to revise to clean up
+		if(Input.GetKeyDown(KeyCode.LeftShift)){
+			walkSpeed = (walkSpeed * .25f);
+		}
+		if (Input.GetKeyUp (KeyCode.LeftShift)) {
+			walkSpeed = (walkSpeed * 4f);
+		}
+	}
+}
