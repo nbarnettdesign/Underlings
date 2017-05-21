5c61c3eb3119ea8128ca499f14458e9523a402bb
nbarnettdesign <nbarnett.design@gmail.com>
2017-05-22 09:57:36 +1000

    Updated Scene populated with buildings

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -35,14 +35,14 @@ public class PlayerController : MonoBehaviour {
 		}
 		//Sneak Function, need to revise to clean up
 		if(Input.GetKeyDown(KeyCode.LeftShift)){
-			walkSpeed = (walkSpeed * .25f);
+			walkSpeed = (walkSpeed * .55f);
 		}
 		if (Input.GetKeyUp (KeyCode.LeftShift)) {
 			walkSpeed = defaultWalkSpeed;
 		}
 		//Run Function
 		if(Input.GetKeyDown(KeyCode.Space)){
-			walkSpeed = .25f ;
+			walkSpeed = .3f ;
 		}
 		if (Input.GetKeyUp (KeyCode.Space)) {
 			walkSpeed = defaultWalkSpeed;
